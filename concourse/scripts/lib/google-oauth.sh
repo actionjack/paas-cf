19b25c4b6593e2b523b55643156e7c8f962c5ef8
henrytk <henry.knott@digital.cabinet-office.gov.uk>
2017-03-14 11:51:54 +0000

    rename google oauth

--- concourse/scripts/lib/google-oauth.sh
+++ concourse/scripts/lib/google-oauth.sh
@@ -2,17 +2,17 @@
 set -e
 set -u
 
-get_gac_secrets() {
+get_google_oauth_secrets() {
   # shellcheck disable=SC2154
-  secrets_uri="s3://${state_bucket}/google-admin-console-secrets.yml"
-  export gac_client_id
-  export gac_client_secret
+  secrets_uri="s3://${state_bucket}/google-oauth-secrets.yml"
+  export oauth_client_id
+  export oauth_client_secret
   if aws s3 ls "${secrets_uri}" > /dev/null ; then
-    secrets_file=$(mktemp -t gac-secrets.XXXXXX)
+    secrets_file=$(mktemp -t google-oauth-secrets.XXXXXX)
 
     aws s3 cp "${secrets_uri}" "${secrets_file}"
-    gac_client_id=$("${SCRIPT_DIR}"/val_from_yaml.rb secrets.google_admin_console_client_id "${secrets_file}")
-    gac_client_secret=$("${SCRIPT_DIR}"/val_from_yaml.rb secrets.google_admin_console_client_secret "${secrets_file}")
+    oauth_client_id=$("${SCRIPT_DIR}"/val_from_yaml.rb secrets.google_oauth_client_id "${secrets_file}")
+    oauth_client_secret=$("${SCRIPT_DIR}"/val_from_yaml.rb secrets.google_oauth_client_secret "${secrets_file}")
 
     rm -f "${secrets_file}"
   fi
