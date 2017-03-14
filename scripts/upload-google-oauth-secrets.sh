d032acd54bd9683b78329f90d110e371471fad14
henrytk <henry.knott@digital.cabinet-office.gov.uk>
2017-03-14 11:52:21 +0000

    rename google oauth

--- scripts/upload-google-oauth-secrets.sh
+++ scripts/upload-google-oauth-secrets.sh
@@ -2,10 +2,10 @@
 
 set -eu
 
-export PASSWORD_STORE_DIR=${GAC_PASSWORD_STORE_DIR}
+export PASSWORD_STORE_DIR=${OAUTH_PASSWORD_STORE_DIR}
 
-GAC_CLIENT_ID=$(pass "google/console/${AWS_ACCOUNT}/client_id")
-GAC_CLIENT_SECRET=$(pass "google/console/${AWS_ACCOUNT}/client_secret")
+OAUTH_CLIENT_ID=$(pass "google/${AWS_ACCOUNT}/oauth/client_id")
+OAUTH_CLIENT_SECRET=$(pass "google/${AWS_ACCOUNT}/oauth/client_secret")
 
 SECRETS=$(mktemp secrets.yml.XXXXXX)
 trap 'rm  "${SECRETS}"' EXIT
@@ -13,8 +13,8 @@ trap 'rm  "${SECRETS}"' EXIT
 cat > "${SECRETS}" << EOF
 ---
 secrets:
-  google_admin_console_client_id: ${GAC_CLIENT_ID}
-  google_admin_console_client_secret: ${GAC_CLIENT_SECRET}
+  google_oauth_client_id: ${OAUTH_CLIENT_ID}
+  google_oauth_client_secret: ${OAUTH_CLIENT_SECRET}
 EOF
 
-aws s3 cp "${SECRETS}" "s3://gds-paas-${DEPLOY_ENV}-state/google-admin-console-secrets.yml"
+aws s3 cp "${SECRETS}" "s3://gds-paas-${DEPLOY_ENV}-state/google-oauth-secrets.yml"
